8d929f00c85f2556579b583ea925c4ba9125b22d
Folasade Ajayi <160342146+SChance444@users.noreply.github.com>
2024-03-19 16:33:59 +0000

    new changes

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require_relative 'app' # Assuming app.rb contains the ScheduleApp class
+
+class TestSchedule < MiniTest::Test
+ def setup
+ @schedule = ScheduleApp.new
+ end
+
+ def test_add_task
+ @schedule.add_task("Task")
+ assert_equal "Task", @schedule.tasks.first
+ end
+
+ def test_add_multiple_task
+ @schedule.add_task("Task 1")
+ @schedule.add_task("Task 2")
+ assert_equal ["Task 1", "Task 2"], @schedule.tasks
+ end
+end
+
+
